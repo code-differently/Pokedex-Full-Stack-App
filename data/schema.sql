9b6c1938d7bca6c8b0d37077ea91db22bbe9f7ab
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-02-24 19:11:50 -0500

    Added Bulbasaur data

--- data/schema.sql
+++ data/schema.sql
@@ -19,3 +19,21 @@ CREATE TABLE IF NOT EXISTS mapType (
 	FOREIGN KEY (pokemonID) REFERENCES pokemon (pokemonID),
 	FOREIGN KEY (typeID) REFERENCES pokeType (typeID)
 );
+
+INSERT INTO
+    pokemon (
+        pokemonID,
+        pokemonName,
+        sprite
+    )
+VALUES
+    (
+        "1",
+        "Bulbasaur",
+        "https://www.https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F2%2F28%2FPok%25C3%25A9mon_Bulbasaur_art.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBulbasaur&tbnid=KtgjmFXIcd8sVM&vet=12ahUKEwjh5ZKN34PvAhX5lXIEHXNuABQQMygAegUIARDlAQ..i&docid=dDlg1E6hw6SY6M&w=275&h=256&q=image%20bulbasaur&ved=2ahUKEwjh5ZKN34PvAhX5lXIEHXNuABQQMygAegUIARDlAQ#imgrc=KtgjmFXIcd8sVM&imgdii=7QuAFbKQuiFkrM"
+    );
+
+INSERT INTO
+    types (typeID, pokemonType)
+VALUES
+    ("1", "grass"); 

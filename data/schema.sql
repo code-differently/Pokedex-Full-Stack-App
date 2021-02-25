603c04cd6c02056ebe85a67286004811f78deb11
Kayanr <kayan.reid@gmail.com>
2021-02-25 14:59:59 -0500

    First commit - add create pokedex database and pokemons, types and pokemon_types tables
    First commit add create pokedex database and pokemons, types and pokemon_types tables.

---
+++ data/schema.sql
@@ -0,0 +1,38 @@
+create database pokedex;
+show databases;
+use pokedex;
+show tables;
+
+describe pokemon;
+
+#drop table pokemon;
+
+CREATE TABLE `pokemons` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `url` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+describe type;
+
+#drop table type;
+CREATE TABLE `types` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `pokemon_types` (
+  `ptid` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `pokemonId` smallint unsigned NOT NULL,
+  `typeId` smallint unsigned NOT NULL,
+  foreign key (pokemonId) references pokemons (id),
+  foreign key (typeId) references types (id),
+  primary key(ptid)
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+

b31ce2e839f21702c5945f108dcbd2b3fbc0a1c8
Anthony Smith <phreedev@Anthonys-MacBook-Air.local>
2021-02-26 11:25:43 -0500

    (Public) Succesful fetch to api

---
+++ web/index.js
@@ -0,0 +1,7 @@
+//Basic promise
+function getPokemon() {
+  fetch("http://localhost:4000/api/v2/allPokemon")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+getPokemon();

21241fd4fd6398e67728a871393c7d9f9aa71f10
Nkenna Ugoanusi <nkanusi@gmail.com>
2021-02-28 21:11:26 -0500

    Connecting to SQL database

--- rest/index.js
+++ rest/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let mysql = require('mysql');
+let password = 'password';
+const cors = require("cors")
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +14,30 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: password,
+  database: "classicmodels"
+});
+
+con.connect(err => {
+  if(err) {
+    throw err;
+  }
+});
+
+app.get('/database', async (req, res) => {
+
+      con.query("SELECT * FROM customers", (err, result, fields) => {
+        if (err){
+            throw err;
+        }
+        console.log(result);
+        res.send(result);
+      });
+
+  
+    
+});

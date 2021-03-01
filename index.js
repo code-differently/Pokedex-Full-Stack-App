c374a6d3413c8813805f7a165cc2812f619e6d5a
seagreenmedia <erinjenkins57@gmail.com>
2021-03-01 02:38:07 -0500

    update

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
+
+let mysql = require('mysql');
+
+let con = mysql = mysql.createConnection({
+    host: "localhost",
+    user: "pokedex",
+    password: "whatever",
+    database: "pokedex"
+});
---
+++ sample.js
@@ -0,0 +1,47 @@
+let password = "0507198Four";
+
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+let mysql = require('mysql');
+
+
+let con = mysql = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "pokedex"
+});
+
+con.connect(err => {
+    if (err) {
+        throw err;
+    }
+    con.query("SELECT * FROM pokemon", (err, result, fields) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+    });
+});
+
+let pokemon_id = 1;
+let name = "Bulbasaur";
+let color = "Green";
+
+//console.log(`INSERT into pokemon (pokemon_id, name, color) VALUES (${pokemon_id}, ${name}, ${color});`);
+con.query(`INSERT into pokemon (pokemon_id, name, color) VALUES (${pokemon_id}, ${name}, ${color});`, (err, result, fields) => {
+    if (err) {
+        throw err;
+    }
+});

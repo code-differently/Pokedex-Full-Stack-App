9a46d76bc04aa0f81ae27d44add5c8d12796f827
Kim Lai <kimlai@Kims-MacBook-Air.local>
2021-03-01 22:25:06 -0500

    store to db

--- rest/index.js
+++ rest/index.js
@@ -1,10 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express()
+app.use(cors())
+const port = 4000
+
+
+
+app.get('/all/:count', async (req, response) => {
+  const count = req.params.count;
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=${count}&offset=0`)
+    .then((res) => res.json())
+    .then((allThePokemon) => {
+        const requestsToMake = allThePokemon.results.map(({ url }) =>
+          fetch(url).then((res) => res.json())
+        );
+        return Promise.all(requestsToMake);
+      })
+    .then(data => {
+      let responseResult = [];
+      for( let i = 0; i < count; i++){
+        let responseObject = { id: data[i].id, name: data[i].name, type: data[i].types[0].type.name, image: data[i].sprites.back_default};
+        responseResult.push(responseObject);
+      }
+      response.json(responseResult);
+    })
+  .catch(err => console.error(err));
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ seedDb.js
@@ -0,0 +1,46 @@
+ const Sequelize = require('Sequelize');
+ const fetch = require('node-fetch');
+
+const typeMap = ({"fire": 2});
+
+ const connection = new Sequelize('pokedex', 'root', 'yourpassword', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+
+  connection.query('show tables').then(console.log);
+
+
+  function addPokemonToDb(data){
+    
+    const {name:pokename,id:pokeid,image} = data;
+    
+      connection.query(`INSERT INTO POKEMON (pokename, ID, sprites) VALUES ("${pokename}","${pokeid}","${image}")`);
+  }
+
+
+  function range(start,end){
+      const arr = [];
+      for(let i =start; i <= end; i++){
+          arr.push(i);
+      }
+      return arr;
+  }
+
+  function getallpokemon(start,end){
+    const endpoint = `https://pokeapi.co/api/v2/pokemon/`
+      return Promise.all(range(start,end).
+      map(id => fetch(`${endpoint}${id}`).
+      then(res =>res.json()).
+      then( ({name,id, sprites:{front_default:image}}) => ({name,id,image}))));
+  }
+
+  getallpokemon(1,67).then(data => data.forEach(addPokemonToDb)).catch(console.error);
+
+  getallpokemon(69,150).then(data => data.forEach(addPokemonToDb)).catch(console.error);

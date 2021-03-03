98fd9d5c3ed1042aadbe9397ed5c50dbb1a76e16
gee92 <boardleygeneara@gmail.com>
2021-03-02 19:50:51 -0500

    full-stackpd

--- rest/index.js
+++ rest/index.js
@@ -1,4 +1,7 @@
-const express = require('express')
+
+
+const express = require('express') // generate express application 
+let mysql = require('mysql');
 const app = express()
 const port = 3000
 
@@ -9,3 +12,31 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+function getCustomerData(){
+
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "classicmodels"
+});
+
+//con.connect(err => {
+//  if(err) {
+ //   throw err;
+//  }
+  con.query("SELECT * FROM customers", (err, result, fields) => { // if error have reject error 
+    if (err){
+        throw err;
+    }
+    console.log(result);
+    con.end();
+  });
+});
+}
+
+let data = getCustomer();
+console.log(data);

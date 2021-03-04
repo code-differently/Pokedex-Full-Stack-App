9b27f516ff806c2ae86469679cfcb96b19dbb25b
ezotic <ezotic@centos8.ezotic.net>
2021-03-04 00:07:36 -0500

    removed sequalize

--- rest/createDatabase.js
+++ rest/createDatabase.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "linux5All##"
+  password: "password"
 });
 
 con.connect(function(err) {

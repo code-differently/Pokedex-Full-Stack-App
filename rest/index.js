b464c7dd5d7e40d71d963336ca0723811a12afe9
Kayanr <kayan.reid@gmail.com>
2021-03-04 16:46:24 -0500

    create xhr object and display 12 pokemon from database on webpage
    create xhr object and display 12 pokemon from database on webpage

--- rest/index.js
+++ rest/index.js
@@ -1,67 +1,41 @@
 //The backend information goes here
-const express = require('express')
+const express = require("express");
 let mysql = require("mysql");
-const app = express()
-const port = 3000
-//let cors = require('cors');
+const app = express();
+const port = 3000;
 
-//app.use(cors());
 
-/*
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-
-// Add Access Control Allow Origin headers
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-*/
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
-/*
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-}) ;
-*/
 
-app.get('/pokemons', (req, res) => {
-  let pokemonData = getPokemonData();
+app.get("/pokemons", async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  let pokemonData = await getPokemonData();
   res.send(pokemonData);
-  //console.log("I am in /pokemons route:" + pokemonData);
-}
-);
-
-async function getPokemonData(){
-let con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password" ,/*remove b4 uploading to gitHub*/
-  database: "pokedex"
+  
 });
 
-let data = await new Promise((resolve, reject) => {
-  con.query("SELECT * FROM pokemons", (err, result, fields) => {
-    (err)? reject(err) : resolve(result);
-    //console.log("I am in Promise section: here are the results" + result);
-})
+async function getPokemonData() {
+  let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password" /*remove b4 uploading to gitHub*/,
+    database: "Pokedex",
+  });
 
-})
-con.end();
-return data;
+  let data = await new Promise((resolve, reject) => {
+    con.query("SELECT * FROM pokemons", (err, result, fields) => {
+      err ? reject(err) : resolve(result);
+    });
+  });
+
+  con.end();
+
+  return data;
 }
--- web/script.js
+++ web/script.js
@@ -1,4 +1,45 @@
-fetch("http://localhost:3000/pokemons").then((response) =>
-  response.json()).then(pokemons => {
-    document.getElementById("test").innerHTML = pokemons.map(pokemon => `<div>${pokemons.id}</div>`).join('')
-  })
+// create an XHR object
+let pokedex = document.getElementById("pokedex");
+
+//console.log(pokedex);
+
+const xhr = new XMLHttpRequest();
+
+
+// listen for `onload` event
+xhr.onload = () => {
+  // process response
+  if (xhr.status == 200) {
+    // parse JSON data
+
+    let pokemonDatas = JSON.parse(xhr.response);
+    
+     displayPokemon(pokemonDatas);
+  } else {
+    console.error("Error!");
+  }
+};
+
+// create a `GET` request
+xhr.open("GET", "http://localhost:3000/pokemons");
+
+// send request
+xhr.send();
+
+const displayPokemon = (pokemon) => {
+  console.log("In displayPokemon" + pokemon);
+
+  const pokemonHTMLString = pokemon.map( pokeman => `
+   <li class="card"> 
+      <h2>${pokeman.name}  <span>#${pokeman.id}</span></h2>
+      <img card="card-title" src="${pokeman.url}"/>
+      
+      <p class="card-subtitle">Type: </p>
+   </li>
+  `
+  ).join('')
+  
+  pokedex.innerHTML = pokemonHTMLString;
+
+}
+

e6b065ad121d1da846026a7b6f4d9b5b1b985e9d
Stephanie Czetli <stephanieczetli@gmail.com>
2021-03-04 20:20:18 -0500

    wrapped db connection & query in function

--- rest/index.js
+++ rest/index.js
@@ -1,40 +1,47 @@
+const password = "myPasswordGoesHere!"  //my pw to access db goes here
+
 const express = require('express')
-const cors = require('cors')
+let mysql = require('mysql')
 const app = express()
-const port = 3000
+const PORT = 3000
+const cors = require('cors')
 
-const password = "myPasswordGoesHere"
-let mysql = require('mysql');
+app.use(cors())  //required to permit cross-origin resource sharing
 
-let con = mysql.createConnection({
-  host: "localhost",
-  user: "root", //your username
-  password: password,
-  database: "classicmodels"
-});
-
-app.use(cors());
-
-con.connect(err => {
-  if(err) {
-    throw err;
-  }
-  con.query("SELECT * FROM customers", (err, result, fields) => {
-    if (err) {
-      throw err;
-    }
-  // console.log(result); //prints results to terminal
-
-    app.get('/customer', (req, res) => { 
-      res.send(result);  //sends results to browser
-    })
-
-  });
-
-});
-
-//Sets up server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${ port }`)
+app.get('/customers', async (req, res) => {
+  let customerData = await getCustomerData();
+  res.send(customerData);
 })
 
+app.listen(PORT, () => { //set up server
+  console.log(`Server is listening at http://localhost:${PORT}`)
+});
+
+async function getCustomerData(){
+  
+  let con = mysql.createConnection({  //open db connection
+    host: "localhost",
+    user: "root", //your username
+    password: password,
+    database: "classicmodels"
+  });
+
+  let data = await new Promise((resolve, reject) => { 
+    con.query("SELECT * FROM customers", (err, result, fields) =>
+    { 
+      if (err) {
+        reject(err)
+      }
+      else { 
+        resolve(result)
+        console.log(result)
+      }
+      //above as ternary - (err) ? reject(err) : resolve(result);
+    })
+  })
+
+  con.end(); //close db connection
+
+  return data;  //from promise above
+
+}
